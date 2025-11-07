3a1fc804109015d64381eb2312246d72be77512a
Nicolas Liautaud <nicolas.liautaud@gmail.com>
2025-11-07 12:53:03 +0100

    fix: limit tracking script to production builds

--- src/main.js
+++ src/main.js
@@ -2,6 +2,15 @@ import { mount } from 'svelte'
 import './app.css'
 import App from './App.svelte'
 
+// inject tracking script in production builds
+if (import.meta.env.PROD) {
+  const s = document.createElement('script')
+  s.defer = true
+  s.src = 'https://stats.nliautaud.fr/script.js'
+  s.setAttribute('data-website-id', '59a85078-d235-4e0b-b0d6-b03c0b7b6f9d')
+  document.head.appendChild(s)
+}
+
 const app = mount(App, {
   target: document.getElementById('app'),
 })
